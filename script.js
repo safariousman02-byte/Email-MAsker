fe8cbb9d99deec0e3fdd9854fb3a1b89bcf2828d
safariousman02-byte <safariousman02@gmail.com>
2026-07-25 23:54:29 +0100

    completing the first step

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+const inputter = document.getElementById("input").value.trim();
+const resutplex = document.getElementById("resultOrg");
+
+const getMasker(inputer) {
+    
+    if (typeof inputer !== "string" || !inputer.includes(@)) {
+        throw new Error("Invalid Email! Please enter a new one");
+    }
+
+}
