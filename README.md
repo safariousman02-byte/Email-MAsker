88377c886bd99fab35342f8a424e1b92092309ec
safariousman02-byte <safariousman02@gmail.com>
2026-07-26 19:54:28 +0100

    adding some modifications

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # BUILSING AN EMAIL MASKER
+
+## step one completing the html part
+
+![the first layout](/src/p1.png)
+
--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 const inputter = document.getElementById("input").value.trim();
 const resutplex = document.getElementById("resultOrg");
 
-const getMasker(inputer) {
+const getMasker = (inputer) => {
     
-    if (typeof inputer !== "string" || !inputer.includes(@)) {
+    if (typeof inputer !== "string" || !inputer.includes("@")) {
         throw new Error("Invalid Email! Please enter a new one");
     }
 
